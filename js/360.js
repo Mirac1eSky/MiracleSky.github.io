629a97203d768c9a6fc1da20fabb869843a63495
Mirac1eSky <923333097@qq.com>
2020-05-06 20:33:47 +0800

    Site updated: 2020-05-06 20:33:40

--- js/360.js
+++ js/360.js
@@ -1,1 +1,4 @@
-document.write('<script src="https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba" id="sozz"><\/script>');
+(function(){
+var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
